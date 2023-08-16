7fef0d38f8022c2abebd32bfd85c13c8e188d997
pepper <adminvcn@icloud.com>
2023-08-16 14:01:22 +0800

    init

---
+++ src/tcp.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace captainstdin\modbus_protocol;
+
+class tcp
+{
+
+
+    /**
+     * 解析具体实例
+     * @param string $buff
+     */
+    public function __construct(string $buff)
+    {
+
+    }
+
+    public function __toString():string
+    {
+        return "";
+    }
+
+
+
+
+}
